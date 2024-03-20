68e0250d32f868fcf8a5980221f9e52ed6a35ea4
Ryan Fan <163138699+tunatoda@users.noreply.github.com>
2024-03-19 21:55:27 -0400

    Add files via upload

---
+++ menuscript.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    // Returning to Menu
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'Backspace' || event.key === 'Backspace') {
+            window.location.href = 'index.html';
+        }
+    });
+}
---
+++ script.js
@@ -0,0 +1,35 @@
+window.onload = function(){
+    // Page selection
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'M' || event.key === 'm') {
+            window.location.href = 'page1.html';
+        }
+        if (event.key === 'W' || event.key === 'w') {
+            window.location.href = 'page2.html';
+        }
+        if (event.key === 'U' || event.key === 'u') {
+            window.location.href = 'page3.html';
+        }
+        if (event.key === 'D' || event.key === 'd') {
+            window.location.href = 'page4.html';
+        }
+        if (event.key === 'A' || event.key === 'a') {
+            window.location.href = 'page5.html';
+        }
+        if (event.key === 'L' || event.key === 'l') {
+            window.location.href = 'page6.html';
+        }
+        if (event.key === 'T' || event.key === 't') {
+            window.location.href = 'page7.html';
+        }
+        if (event.key === 'I' || event.key === 'i') {
+            window.location.href = 'page8.html';
+        }
+        if (event.key === 'N' || event.key === 'n') {
+            window.location.href = 'page9.html';
+        }
+        if (event.key === 'F' || event.key === 'f') {
+            window.location.href = 'page10.html';
+        }
+    });
+}
